4d6c3be3d17c63a482485f074cf4e7cdc0a77329
Aliaksei Melnikau <v-amelnikau@expedia.com>
2018-01-28 22:19:21 +0300

    Fix log file folder

--- src/log/logger.js
+++ src/log/logger.js
@@ -1,10 +1,11 @@
 const winston = require('winston');
+const appRoot = require('app-root-path');
 
 const logger = new (winston.Logger) ({
     level: 'info',
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: '../blog.log' })
+        new winston.transports.File({ filename: appRoot + '/blog.log' })
     ]
 });
 
